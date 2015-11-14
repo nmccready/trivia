19e233889a102fbd2d0075d1aa3a392a16ed86a3
Nick McCready <nmccready@gmail.com>
2015-11-14 14:26:48 -0500

    scala and coffee progress

---
+++ scala/src/test/scala/com/adaptionsoft/games/trivia/runner/GameRunnerSpec.scala
@@ -0,0 +1,6 @@
+package com.adaptionsoft.games.trivia.runner
+
+import com.adaptionsoft.games.uglytrivia.Game
+import java.util.Random
+
+
---
+++ scala/src/test/scala/com/adaptionsoft/games/uglytrivia/GameSpec.scala
@@ -0,0 +1,15 @@
+package com.adaptionsoft.games.uglytrivia
+
+import java.util.{LinkedList, ArrayList}
+import org.scalatest._
+
+class GameSpec extends FlatSpec with Matchers {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+
+  }
+}
